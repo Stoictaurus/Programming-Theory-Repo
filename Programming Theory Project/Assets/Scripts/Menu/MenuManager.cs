20742356b811f076dbdf7991adec114c9999cd93
Stoictaurus <stoictaurus@gmail.com>
2021-12-21 08:16:14 +0100

    Implement basic menu

---
+++ Programming Theory Project/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private InputField inputName;
+
+    public void StartGameAction()
+    {
+        // Nothing here yet
+        Debug.Log("Start game!");
+    }
+}
