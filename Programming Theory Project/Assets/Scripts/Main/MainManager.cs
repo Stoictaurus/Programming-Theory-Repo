bdabe1c8f875b07b8b99425271b92b469648d2e8
Stoictaurus <stoictaurus@gmail.com>
2021-12-21 11:56:54 +0100

    PlayerStatus Display

--- Programming Theory Project/Assets/Scripts/Main/MainManager.cs
+++ Programming Theory Project/Assets/Scripts/Main/MainManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
+    [SerializeField] private Text playerName;
+    [SerializeField] private Text hungerText;
+    [SerializeField] private Text restText;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -11,6 +15,20 @@ public class MainManager : MonoBehaviour
             GameManager.Instance.ExitToMenu();
         }
     }
-    
-    
+
+    public void DisplayPlayerStatus(GameObject player)
+    {
+        var playerScript = player.GetComponent<Player>();
+        if (playerScript != null)
+        {
+            playerName.text = playerScript.name;
+            hungerText.text = "Hunger: " + playerScript.hunger;
+            restText.text = "Rest   : " + playerScript.rest;
+        }
+        else
+        {
+            hungerText.text = "Hunger: -";
+            restText.text = "Rest   : -";
+        }
+    }
 }
--- Programming Theory Project/Assets/Scripts/Main/Player.cs
+++ Programming Theory Project/Assets/Scripts/Main/Player.cs
@@ -6,21 +6,51 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private RoomBounds roomBounds = new RoomBounds(); //Abstraction
+    private MainManager _manager;
+    
     [SerializeField] private float speed = 5.0f;
+    public int hunger { get; private set; } = 100; //Encapsulation
+    public int rest { get; private set; } = 100; //Encapsulation
     void Start()
     {
         roomBounds.bottom = -10f;
         roomBounds.top = 10f;
         roomBounds.left = -18.5f;
         roomBounds.right = 18.5f;
+
+        _manager = FindObjectOfType<MainManager>();
+        StartCoroutine(TimePass());
     }
 
+    IEnumerator TimePass()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            LowerPlayerNeeds();
+            _manager.DisplayPlayerStatus(gameObject);
+        }
+    }
 
     void Update()
     {
         Move();
     }
 
+    private void LowerPlayerNeeds() //Abstraction
+    {
+        hunger--;
+        rest--;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
+
+        if (rest < 0)
+        {
+            rest = 0;
+        }
+    }
     private void Move() //Abstraction
     {
         var horizontal = Input.GetAxis("Horizontal");
